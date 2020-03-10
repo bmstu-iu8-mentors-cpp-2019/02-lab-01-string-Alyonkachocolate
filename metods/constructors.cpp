71d289fad976d32b503249bd9971428d51365cab
user <Nyanyoka@yandex.ri>
2020-03-10 18:29:36 +0300

    Update constructors

--- metods/constructors.cpp
+++ metods/constructors.cpp
@@ -5,16 +5,15 @@
 String::~String() { delete[] data; }
 
 /// Конструктор по умолчанию
-String::String() : length(0) { data = new char[0]; }
+String::String() : length(0), data(new char[0]) {}
 
 /// Конструктор копирования
-String::String(const String &rhs) : length(rhs.length) {
-  data = new char[length];
-  for (size_t i = 0; i < length; i++) data[i] = rhs.data[i];
+String::String(const String &rhs) : length(rhs.length), data(new char[length]) {
+  std::copy(rhs.data, rhs.data + length, data);
 }
 
 /// Пользовательский конструктор
-String::String(const char *data1) : length(strlen(data1)) {
-  data = new char[length];
-  for (size_t i = 0; i < length; i++) data[i] = data1[i];
+String::String(const char *data1)
+    : length(strlen(data1)), data(new char[length]) {
+  std::copy(data1, data1 + length, data);
 }
